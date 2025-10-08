afa04c5183058f1dd6f8a0e9cdcc988d14f349cc
jasmine-codes <jsmlee97@gmail.com>
2025-10-08 10:53:41 +0200

    menu setup

--- Program.cs
+++ Program.cs
@@ -8,4 +8,21 @@ do
     Console.Clear();
     Console.WriteLine("Welcome to Sip and Glow✨");
     Console.WriteLine("--------------------------");
-}
+    Console.WriteLine("1) Set daily goal");
+    Console.WriteLine("2) Log a glass of water");
+    Console.WriteLine("3) View progress");
+    Console.WriteLine("4) Reset progress");
+    Console.WriteLine("5) Exit");
+    Console.WriteLine();
+    Console.Write("Select an option (1-5): ");
+
+    readResult = Console.ReadLine();
+
+    if (readResult != null) menuSelection = readResult.ToLower();
+
+    switch (menuSelection)
+    {
+        
+    }
+
+} while (menuSelection != "5");
